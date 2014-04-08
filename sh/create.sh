eefd2d2d454be9c6dd5e167deca6a7f598ea6ff8
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2014-04-08 17:42:17 +0200

    fix typos

--- sh/create.sh
+++ sh/create.sh
@@ -14,11 +14,11 @@ then
 fi
 
 
-AMI=$(/home/ec2-user/AMIFinder/findAMI.py -a ${Event_ResourceProperties_Version})
+AMI=$(/home/ec2-user/findAMI.py -a ${Event_ResourceProperties_Version})
 AMI_ret=$?
 if [ $AMI_ret -ne 0 ]
 then
-    error_exit "finAMI.py failed." $AMI_ret
+    error_exit "findAMI.py failed." $AMI_ret
 else
     echo "{ \"PhysicalResourceId\" : \"$AMI\" }" >&3 3>&-  # echo success to stdout (instead of log) and then close fd 3
     exit 0
